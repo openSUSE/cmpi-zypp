581ffdd341700cd3ba45caba284093f9f50d78ca
Michael Calmer <mc@suse.de>
2009-01-29 16:36:19 +0100

    fix indenting

--- src/SUSE_AssocFilter.h
+++ src/SUSE_AssocFilter.h
@@ -7,20 +7,20 @@ namespace cmpizypp
 {
   class SUSE_AssocFilter
   {
-  public:
-    SUSE_AssocFilter() {};
-    virtual ~SUSE_AssocFilter() {};
+    public:
+      SUSE_AssocFilter() {};
+      virtual ~SUSE_AssocFilter() {};
 
-    virtual bool filterInstance(const CmpiObjectPath &scop, const char * _RefSource, const CmpiInstance &ci, bool associators) const
-    {
-      _CMPIZYPP_TRACE(1,("--- AssocFilter::filterInstance() called"));
-      return true;
-    };
-    virtual bool filterObjectPath(const CmpiObjectPath &scop, const char * _RefSource, const CmpiObjectPath &op, bool associators) const
-    {
-      _CMPIZYPP_TRACE(1,("--- AssocFilter::filterObjectPath() called"));
-      return true;
-    }
+      virtual bool filterInstance(const CmpiObjectPath &scop, const char * _RefSource, const CmpiInstance &ci, bool associators) const
+      {
+        _CMPIZYPP_TRACE(1,("--- AssocFilter::filterInstance() called"));
+        return true;
+      };
+      virtual bool filterObjectPath(const CmpiObjectPath &scop, const char * _RefSource, const CmpiObjectPath &op, bool associators) const
+      {
+        _CMPIZYPP_TRACE(1,("--- AssocFilter::filterObjectPath() called"));
+        return true;
+      }
   };
 
 } // namespace cmpizypp
